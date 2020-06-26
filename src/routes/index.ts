1cd15739dc840ea8b94f1fc61e62808b2cfddf2b
brunolimadev <bruno.lima.dev@outlook.com>
2020-06-26 12:04:09 -0300

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+import appointmentsRouter from './appointments.routes'
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+
+
+export default routes;

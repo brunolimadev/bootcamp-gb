4d5d06803dc1166011326c66b54c0a4c68bbeabf
brunolimadev <bruno.lima.dev@outlook.com>
2020-06-26 12:13:37 -0300

    Separation of concerns

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, request, response } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -6,6 +6,11 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+    return response.json(appointments);
+})
 
 appointmentsRouter.post('/', (request, response) => {
 
